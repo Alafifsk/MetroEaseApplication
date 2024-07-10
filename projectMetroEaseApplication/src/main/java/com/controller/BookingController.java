bacd41ad3304cd9a7e207a545bf4312b88598e1d
AL AFIF SHAIKH <AL AFIF SHAIKH@LAPTOP-8FJHJN3V>
2024-07-10 16:32:00 +0530

    first commit

---
+++ projectMetroEaseApplication/src/main/java/com/controller/BookingController.java
@@ -0,0 +1,62 @@
+package com.controller;
+
+import java.util.List;
+import java.util.Map;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.exception.BookingNotFoundException;
+import com.model.Booking;
+import com.service.BookingService;
+
+@RestController
+public class BookingController {
+
+	@Autowired
+	BookingService bookingService;
+	
+	@PostMapping("/addBooking")
+	public ResponseEntity<Booking> AddBooking(@RequestBody Booking booking)
+	{
+		Booking b = bookingService.saveBooking(booking);
+		return ResponseEntity.status(HttpStatus.CREATED).header("booking","booking done").body(b);
+	}
+	
+	
+	@GetMapping("/getBooking/{id}")
+	public ResponseEntity<Map<String, Object>> getOneBooking(@PathVariable long id) throws BookingNotFoundException
+	{
+		Map<String, Object> response = bookingService.getBookingById(id);
+		
+		return ResponseEntity.ok(response);
+	}
+	
+	@GetMapping("/getAllBooking")
+	public List<Booking> getAllBooking(){
+		return bookingService.getAllBooking();
+		
+	}
+	
+	@PutMapping("/updateBooking")
+	public ResponseEntity<Booking> updateBooking(@RequestBody Booking booking){
+		
+		Booking b = bookingService.updateBooking(booking);
+		return ResponseEntity.status(HttpStatus.ACCEPTED).header("booking", "booking updated").body(b);
+	}
+	
+	@DeleteMapping("/deleteBooking/{id}")
+	public ResponseEntity<Map<String, Object>> cancelBooking(@PathVariable ("id") long id) throws BookingNotFoundException{
+		Map<String, Object> response = bookingService.deleteBooking(id);
+		
+		return ResponseEntity.ok(response);
+	}
+}
---
+++ projectMetroEaseApplication/src/test/java/com/demo/projectMetroEaseApplication/ProjectMetroEaseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.demo.projectMetroEaseApplication;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectMetroEaseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
